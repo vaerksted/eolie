91d8845758856e0d5111aa8af2241c0e42c99a60
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2017-05-06 01:28:28 +0200

    Do not update snapshot if view is loading

--- src/stacksidebar_child.py
+++ src/stacksidebar_child.py
@@ -298,7 +298,8 @@ class SidebarChild(Gtk.ListBoxRow):
         """
         if self.__scroll_timeout_id is not None:
             GLib.source_remove(self.__scroll_timeout_id)
-        self.__scroll_timeout_id = GLib.timeout_add(
+        if not self.view.is_loading():
+            self.__scroll_timeout_id = GLib.timeout_add(
                                                 1000,
                                                 self.__set_snapshot_timeout)
 
