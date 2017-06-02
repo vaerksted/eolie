1035984d83e7a791efd1363a8a48ba6d07a44bb7
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2017-06-02 23:39:09 +0200

    Update comment

--- src/menu_pages.py
+++ src/menu_pages.py
@@ -74,7 +74,7 @@ class PagesMenu(Gio.Menu):
             # FIXME
             El().history.get_opened_pages()
         else:
-            # Delayed to let webkit db initialize
+            # Delayed to not slow down startup
             GLib.timeout_add(1000, self.__append_opened_pages)
 
     def activate_last_action(self):
