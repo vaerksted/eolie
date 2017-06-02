8ec811a9262e7cb13846521a2b68c65b0c6f6751
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2017-06-02 05:01:11 +0200

    Use after setting state

--- src/view_web_navigation.py
+++ src/view_web_navigation.py
@@ -342,8 +342,8 @@ class WebViewNavigation:
                 decision.ignore()
                 return True
             else:
-                decision.use()
                 El().history.set_page_state(webview.get_uri())
+                decision.use()
                 return False
         else:
             self.emit("new-page", uri, Gdk.WindowType.OFFSCREEN)
