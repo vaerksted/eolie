e913520236ea4179a3568c1295dc6e6be5884c08
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2017-08-22 13:02:23 +0200

    Add missing args

--- src/pages_manager_flowbox_child.py
+++ src/pages_manager_flowbox_child.py
@@ -55,7 +55,7 @@ class PagesManagerFlowBoxChild(Gtk.FlowBoxChild, PagesManagerChild):
             @param uri as str
             @param save as bool
         """
-        PagesManagerChild.set_snapshot(self)
+        PagesManagerChild.set_snapshot(self, uri, save)
         if self._view.webview.ephemeral:
             self._image.set_from_icon_name(
                                          "user-not-tracked-symbolic",
--- src/pages_manager_listbox_child.py
+++ src/pages_manager_listbox_child.py
@@ -80,7 +80,7 @@ class PagesManagerListBoxChild(Gtk.ListBoxRow, PagesManagerChild):
             @param uri as str
             @param save as bool
         """
-        PagesManagerChild.set_snapshot(self)
+        PagesManagerChild.set_snapshot(self, uri, save)
         if self._view.webview.ephemeral:
             panel_mode = El().settings.get_enum("panel-mode")
             if panel_mode != PanelMode.MINIMAL:
