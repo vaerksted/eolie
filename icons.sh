366de11c949f7bc18e45d114f4f0ed65ccb10022
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2017-10-31 09:50:57 +0100

    Move icon

--- icons.sh
+++ icons.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in 16 22 32 48 256 512; do
-	inkscape -z -e data/icons/hicolor/$ix$i/apps/org.gnome.Eolie.png -w $i -h $i data/icons/hicolor/org.gnome.Eolie.svg
+	inkscape -z -e data/icons/hicolor/$ix$i/apps/org.gnome.Eolie.png -w $i -h $i data/icons/hicolor/scalable/apps/org.gnome.Eolie.svg
 done
