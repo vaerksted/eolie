1d183bd483cfaa4f45e60f1715c1b49c16dfdc31
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2017-11-09 20:04:47 +0100

    Add missing copyright for Server class

--- eolie/extension_proxy.py
+++ eolie/extension_proxy.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2017 Cedric Bellegarde <cedric.bellegarde@adishatz.org>
+# Copyright (c) 2016 Felipe Borges <felipeborges@gnome.org>
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
