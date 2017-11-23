be3171b43fbded85f147bb652d419db1759d918d
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2017-11-23 15:52:52 +0100

    Profile switching handler

--- eolie/webview_navigation.py
+++ eolie/webview_navigation.py
@@ -422,11 +422,21 @@ class WebViewNavigation:
                 decision.ignore()
                 return True
             else:
-                uri = webview.uri
-                El().history.set_page_state(uri)
+                navigation_type = navigation_action.get_navigation_type()
+                El().history.set_page_state(self._navigation_uri)
                 self._error = None
-                decision.use()
-                return False
+                # Do not stop loading here because needed by form
+                # May lead to GObject warnings about cookies
+                if navigation_type in [
+                                   WebKit2.NavigationType.FORM_SUBMITTED,
+                                   WebKit2.NavigationType.FORM_RESUBMITTED]:
+                    decision.use()
+                    return False
+                # Force using load_uri() to prevent errors in profile switching
+                else:
+                    decision.ignore()
+                    self.load_uri(self._navigation_uri)
+                    return True
         else:
             self.new_page(LoadingType.BACKGROUND)
             decision.ignore()
