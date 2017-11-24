d02832a43f30e75e169ad58bbef18942c8a02880
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2017-11-24 14:48:27 +0100

    Add missing file

---
+++ data/Extensions.js
@@ -0,0 +1,11 @@
+window.external={};
+window.external.AddSearchProvider = function(uri)
+{
+    alert("@EOLIE_OPENSEARCH@".concat(uri));
+
+};
+window.external.addOpenSearch = function(uri)
+{
+    alert("@EOLIE_OPENSEARCH@".concat(uri));
+
+};
