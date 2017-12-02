c541e1cb495e9710b1d7f97950dddbc6d2dd824c
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2017-12-02 17:36:13 +0100

    Clean up

--- eolie/extension_jsblock.py
+++ eolie/extension_jsblock.py
@@ -79,5 +79,4 @@ class JSblockExtension:
                 for i in range(0, self.__scripts.get_length()):
                     script = self.__scripts.item(i)
                     if script.get_src() == request_uri:
-                        print(request_uri)
                         return True
