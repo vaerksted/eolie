20b498921a6ce7445ce0a361238270a295cecc6e
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2017-12-15 19:00:22 +0100

    Fix default zoom level for related webviews

--- eolie/webview.py
+++ eolie/webview.py
@@ -95,7 +95,7 @@ class WebView(WebKit2.WebView):
                 zoom_level *= self._window.zoom_level
             else:
                 window = self.__related_view.get_ancestor(Gtk.Window)
-                if window is not None and hasattr(window, "zoom-level"):
+                if window is not None and hasattr(window, "zoom_level"):
                     zoom_level *= window.zoom_level
         except Exception as e:
             print("WebView::update_zoom_level()", e)
