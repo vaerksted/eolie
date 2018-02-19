c10d419bdf61e2bb902d58a3be7005b7bce08337
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2018-02-19 10:10:49 +0100

    Update (c)

--- eolie/extension_proxy.py
+++ eolie/extension_proxy.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Cedric Bellegarde <cedric.bellegarde@adishatz.org>
+# Copyright (c) 2017-2018 Cedric Bellegarde <cedric.bellegarde@adishatz.org>
 # Copyright (c) 2016 Felipe Borges <felipeborges@gnome.org>
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
