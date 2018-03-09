7e87609d6545ccf63c87bf250b2c601d7250684d
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2018-03-09 10:45:17 +0100

    Load new pages in background

--- eolie/menu_pages.py
+++ eolie/menu_pages.py
@@ -159,7 +159,7 @@ class PagesMenu(Gio.Menu):
             @param GVariant
         """
         App().active_window.container.add_webview(App().start_page,
-                                                  LoadingType.FOREGROUND,
+                                                  LoadingType.BACKGROUND,
                                                   True)
 
     def __on_openall_clicked(self, action, variant):
