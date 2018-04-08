2c7823dc75ebfa78592d3b0e29f0846c37e3f43b
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2018-04-08 21:44:30 +0200

    Fix typo

--- eolie/container.py
+++ eolie/container.py
@@ -270,7 +270,7 @@ class Container(Gtk.Overlay):
             @param expose as bool
         """
         if expose:
-            self.__pages_manager.update_sort(self.__sites_manager.get_sort())
+            self.__pages_manager.update_sort(self.__sites_manager.sort)
             self.__pages_manager.set_filtered(True)
         else:
             self.__window.toolbar.actions.view_button.set_active(False)
