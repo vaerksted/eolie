1dd1fc5b4e67cbb2a54367bc47a168e919f01501
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2018-04-19 14:54:18 +0200

    Needed on start

--- eolie/webview_navigation.py
+++ eolie/webview_navigation.py
@@ -153,7 +153,7 @@ class WebViewNavigation:
         """
         parsed = urlparse(self._uri)
         if event == WebKit2.LoadEvent.STARTED:
-            pass
+            self.emit("uri-changed", self._uri)
         elif event == WebKit2.LoadEvent.REDIRECTED:
             pass
         elif event == WebKit2.LoadEvent.COMMITTED:
