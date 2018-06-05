b1f55cf432b3b3087ee502f3ee5256815f69e32f
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2018-06-05 21:40:01 +0200

    Update history on js update

--- eolie/extension_proxy.py
+++ eolie/extension_proxy.py
@@ -72,7 +72,7 @@ class Server:
                 invocation.return_value(variant)
             else:
                 invocation.return_value(None)
-        except Exception as e:
+        except:
             pass
 
 
--- eolie/webview_signals.py
+++ eolie/webview_signals.py
@@ -10,7 +10,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import Gtk, Gdk, Gio, WebKit2, GObject
+from gi.repository import Gtk, Gdk, Gio, WebKit2, GObject, GLib
 
 from gettext import gettext as _
 from urllib.parse import urlparse
@@ -54,6 +54,7 @@ class WebViewSignals(WebViewMenuSignals, WebViewJsSignals,
         WebViewJsSignals.__init__(self)
         WebViewDBusSignals.__init__(self)
         WebViewLoadSignals.__init__(self)
+        self.__title_changed_timeout_id = None
         self.reset_last_click_event()
         self.__cancellable = Gio.Cancellable()
         self.connect("map", self._on_map)
@@ -188,6 +189,16 @@ class WebViewSignals(WebViewMenuSignals, WebViewJsSignals,
             @param webview as WebView
             @param uri as str
         """
+        def title_changed_timeout():
+            """
+                We need this to update history as soon title is available
+            """
+            self.__title_changed_timeout_id = None
+            self.__on_title_changed(webview, webview.title)
+        if self.__title_changed_timeout_id is not None:
+            GLib.source_remove(self.__title_changed_timeout_id)
+        self.__title_changed_timeout_id = GLib.timeout_add(
+            2000, title_changed_timeout)
         self.__cancellable.cancel()
         self.__cancellable.reset()
 
@@ -197,6 +208,9 @@ class WebViewSignals(WebViewMenuSignals, WebViewJsSignals,
             @param webview as WebView
             @param title as str
         """
+        if self.__title_changed_timeout_id is not None:
+            GLib.source_remove(self.__title_changed_timeout_id)
+        self.__title_changed_timeout_id = None
         parsed = urlparse(webview.uri)
         if self.error or\
                 webview.ephemeral or\
