43c73c24fdb83ff897106c85d9ad2b31ab41ba98
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2019-11-18 11:35:31 +0100

    Fix password saving for id.fedoraproject.org.

--- data/javascript/FormMenu.js
+++ data/javascript/FormMenu.js
@@ -5,15 +5,25 @@ formsArrayMenu.forEach(function(form) {
     inputsArrayMenu = form.querySelectorAll("input");
     inputsArrayMenu.forEach(function(input) {
         const types = ["text", "email", "search"];
+        const names = ["login", "user", "email"];
         let name = input.getAttribute("name");
         let type = input.getAttribute("type");
-        if (types.includes(type) && name !== null) {
-            user_form_names = user_form_names.concat(input.name, "\n");
-            input.addEventListener("click", function() {
-                message = "@EOLIE_FORM_MENU_MESSAGE@\n";
-                message += input.name;
-                alert(message);
-            });
+        let valid_type = types.includes(type);
+        let valid_name = false;
+        if (name !== null) {
+            for (let i = 0; i < names.length; i++) {
+                if (name.search(names[i]) != -1) {
+                    valid_name = true;
+                }
+            }
+            if (type != "password" && (valid_type || valid_name)) {
+                user_form_names = user_form_names.concat(input.name, "\n");
+                input.addEventListener("click", function() {
+                    message = "@EOLIE_FORM_MENU_MESSAGE@\n";
+                    message += input.name;
+                    alert(message);
+                });
+            }
         }
     });
 });
--- data/javascript/Submit.js
+++ data/javascript/Submit.js
@@ -3,6 +3,7 @@ let elementsArray = document.querySelectorAll("form");
 elementsArray.forEach(function(elem) {
     elem.addEventListener("submit", function() {
         const types = ["text", "email", "search"];
+        const names = ["login", "user", "email"];
         let form_uri = elem.action
         let username_input_name;
         let username_input_value;
@@ -13,16 +14,25 @@ elementsArray.forEach(function(elem) {
             let input = inputsArray[i];
             let name = input.getAttribute("name");
             let type = input.getAttribute("type");
-            if (types.includes(type) && name !== null) {
-                username_input_name = input.name;
-                username_input_value = input.value;
-            }
-            if (type == "password") {
-                password_input_name = input.name;
-                password_input_value = input.value;
-            }
-            if (password_input_name !== undefined && username_input_name !== undefined) {
-                break;
+            let valid_type = types.includes(type);
+            let valid_name = false;
+            if (name !== null) {
+                for (let i = 0; i < names.length; i++) {
+                    if (name.search(names[i]) != -1) {
+                        valid_name = true;
+                    }
+                }
+                if (type != "password" && (valid_type || valid_name)) {
+                    username_input_name = input.name;
+                    username_input_value = input.value;
+                }
+                if (type == "password") {
+                    password_input_name = input.name;
+                    password_input_value = input.value;
+                }
+                if (password_input_name !== undefined && username_input_name !== undefined) {
+                    break;
+                }
             }
         }
         let message = "@EOLIE_SUBMIT@\n";
