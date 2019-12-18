02c4c8eacb5d6b6e684398ba213447e2017f26f9
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2019-12-18 19:02:17 +0100

    Remove unwanted gradient in start.html

--- eolie/webview_night_mode.py
+++ eolie/webview_night_mode.py
@@ -42,6 +42,10 @@ class WebViewNightMode:
                         color: #EAEAEA !important;\
                         background-color: #353535 !important\
                       }\
+                      *[style] {\
+                        color: #EAEAEA !important;\
+                        background: #353535 !important;\
+                      }\
                       a {\
                         color: #F0FFFE !important;\
                       }\
