9e8a07029a20a45b905a1519835948b957adc6d2
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2020-02-27 00:51:15 +0100

    Add missing dir

---
+++ bin/generate_data.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function generate_resource()
+{
+    echo '<?xml version="1.0" encoding="UTF-8"?>'
+    echo '<gresources>'
+    echo '  <gresource prefix="/org/gnome/Eolie">'
+    for file in data/*.html data/*.css data/*.js
+    do
+        echo -n '    <file compressed="true">'
+        echo -n $(basename $file)
+        echo '</file>'
+    done
+    for file in data/javascript/*
+    do
+        echo -n '    <file compressed="true">'
+        echo -n javascript/$(basename $file)
+        echo '</file>'
+    done
+    for file in data/*.ui AboutDialog.ui
+    do
+        echo -n '     <file compressed="true" preprocess="xml-stripblanks">'
+        echo -n $(basename $file)
+        echo '</file>'
+    done
+    echo '  </gresource>'
+    echo '</gresources>'
+}
+
+function generate_po()
+{
+    cd po
+    git pull https://hosted.weblate.org/git/gnumdk/eolie
+    >eolie.pot
+    for file in ../data/org.gnome.Eolie.gschema.xml ../data/*.in ../data/*.ui ../eolie/*.py
+    do
+        xgettext --from-code=UTF-8 -j $file -o eolie.pot
+    done
+    >LINGUAS
+    for po in *.po
+    do
+        msgmerge -N $po eolie.pot > /tmp/$$language_new.po
+        mv /tmp/$$language_new.po $po
+        language=${po%.po}
+        echo $language >>LINGUAS
+    done
+}
+
+generate_resource > data/eolie.gresource.xml
+generate_po
